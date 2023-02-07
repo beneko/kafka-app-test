72889119d2fa7f3859eb01cc0ca37191d92ca3e1
BEHZAD NEKOUEI <behzad.nekouei@atos.net>
2023-02-07 09:45:59 +0100

    update Consumer

--- src/main/java/ConsumerApp.java
+++ src/main/java/ConsumerApp.java
@@ -2,20 +2,17 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.IntegerDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
-import java.util.concurrent.Executors;
 
 public class ConsumerApp {
     public static void main(String[] args) {
         Properties properties = new Properties();
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group-1");
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.IntegerDeserializer");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
         properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
@@ -25,6 +22,7 @@ public class ConsumerApp {
         KafkaConsumer<Integer, String> consumer = new KafkaConsumer<>(properties);
         consumer.subscribe(Collections.singletonList("test-topic-1"));
 
+        // This is a way to poll the messages in a scheduled way
 //        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> {
 //            ConsumerRecords<Integer, String> records = consumer.poll(Duration.ofMillis(1000));
 //            for (ConsumerRecord<Integer, String> record : records) {
@@ -32,11 +30,16 @@ public class ConsumerApp {
 //            }
 //        }, 0, 1, java.util.concurrent.TimeUnit.SECONDS );
 
-        while (true) {
-            ConsumerRecords<Integer, String> records = consumer.poll(Duration.ofMillis(1000));
-            for (ConsumerRecord<Integer, String> record : records) {
-                System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
+        // This is a way to poll the messages in a loop
+        try {
+            while (true) {
+                ConsumerRecords<Integer, String> records = consumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord<Integer, String> record : records) {
+                    System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
+                }
             }
+        } finally {
+            consumer.close();
         }
     }
 }
